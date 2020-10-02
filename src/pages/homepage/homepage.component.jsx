2cd16e2ed3e913d392b14820d27f2e5eaa477333
adiwawan90 <adi.wawan90@gmail.com>
2020-10-02 10:50:29 +0700

    membuat component lebih kecil dan terpisah, styling menu-item

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Directory from '../../component/directory/directory.component';
 import './homepage.style.scss';
 
 const HomePage = () => {
     return (
         <div className="homepage">
-            <div className="directory-menu">
+            {/* <div className="directory-menu">
                 <div className="menu-item">
                     <div className="content">
                         <h1 className="title">HATS</h1>
@@ -35,7 +36,8 @@ const HomePage = () => {
                         <span className="subtitle">Shop Now</span>
                     </div>
                 </div>
-            </div>
+            </div> */}
+            <Directory />
         </div>
     )
 }
